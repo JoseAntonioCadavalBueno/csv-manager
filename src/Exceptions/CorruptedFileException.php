f8dc1d08653066f41d8a71883ffaab813482bcf0
Jose antonio Cadaval Bueno <jcadavalbueno@gmail.com>
2025-11-02 11:38:45 +0000

    Resolve "feature: Add Marker Interface for All Library Exceptions"

--- src/Exceptions/CorruptedFileException.php
+++ src/Exceptions/CorruptedFileException.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace CsvManager\Exceptions;
+use CsvManager\Contracts\ICsvException;
 use Exception;
 use CsvManager\Core\LanguageManager;
 use Throwable;
 
-class CorruptedFileException extends Exception
+class CorruptedFileException extends Exception implements ICsvException
 {
     public function __construct(?string $message = null, int $code = 415, ?Throwable $previous = null)
     {
--- src/Exceptions/OverflowException.php
+++ src/Exceptions/OverflowException.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace CsvManager\Exceptions;
+use CsvManager\Contracts\ICsvException;
 use Exception;
 use CsvManager\Core\LanguageManager;
 use Throwable;
 
-class OverflowException extends Exception
+class OverflowException extends Exception implements ICsvException
 {
     public function __construct(?string $message = null, int $code = 500, ?Throwable $previous = null)
     {
